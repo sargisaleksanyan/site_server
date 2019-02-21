1596ab7e4193e85022e6a4ff1bb480f642b623ea
sargis aleksanyan <sarqisjob2012@gmail.com>
2019-02-21 21:51:07 +0400

    Update

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 ﻿const express = require("express");
 const app = express();
-const port = process.env.PORT | 3001;
+const port = process.env.PORT || 3001;
 app.use(express.static(__dirname + "/public"));
 
 
