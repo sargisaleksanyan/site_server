da620f4102c7b789116d58d77b2c5c04b5b0863e
sargis aleksanyan <sarqisjob2012@gmail.com>
2019-02-24 22:15:07 +0400

    Update

--- Db_config.js
+++ Db_config.js
@@ -1,16 +1,18 @@
 
 module.exports = {
+    host: "sql9.freemysqlhosting.net",
+    user: "sql9280560",
+    password: "WRqxYUI7Pq",
+    database: "sql9280560"
+};
+
+/*
+module.exports = {
     host: "remotemysql.com",
     user: "8fRIFsyncZ",
     password: "Y94pAa3Nvw",
     database: "8fRIFsyncZ"
 };
 
+*/
 
-/*
-module.exports = {
-    host: "localhost",
-    user: "root",
-    password: "hovik2011",
-    database: "sites_visit"
-};*/
